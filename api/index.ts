93a31ffeb27d4afc0d9101817976ed856b8a3ca3
Rohit Yadav <thesandman.sh@gmail.com>
2026-03-26 09:50:47 +0530

    Fix Vercel API handler typing

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,18 @@
 type RequestLike = unknown;
 type ResponseLike = unknown;
 type ExpressHandler = (req: RequestLike, res: ResponseLike) => unknown;
+type ExpressAppLike = {
+  use: (...args: unknown[]) => void;
+  listen: (port: number, cb: (err?: unknown) => void) => void;
+};
 
 let appPromise: Promise<ExpressHandler> | null = null;
 
 function loadApp(): Promise<ExpressHandler> {
   if (!appPromise) {
-    appPromise = import("../apps/api/src/app.js").then((module) => module.default as ExpressHandler);
+    appPromise = import("../apps/api/src/app.js").then(
+      (module) => module.default as unknown as ExpressAppLike as unknown as ExpressHandler,
+    );
   }
   return appPromise;
 }
