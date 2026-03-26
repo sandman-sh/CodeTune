d905ebeb09bd55faebf4f063e57dd434a3439288
Rohit Yadav <thesandman.sh@gmail.com>
2026-03-26 09:47:31 +0530

    Fix Vercel API ESM loading

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,17 @@
-import app from "../apps/api/src/app.js";
+type RequestLike = unknown;
+type ResponseLike = unknown;
+type ExpressHandler = (req: RequestLike, res: ResponseLike) => unknown;
 
-export default app;
+let appPromise: Promise<ExpressHandler> | null = null;
+
+function loadApp(): Promise<ExpressHandler> {
+  if (!appPromise) {
+    appPromise = import("../apps/api/src/app.js").then((module) => module.default as ExpressHandler);
+  }
+  return appPromise;
+}
+
+export default async function handler(req: RequestLike, res: ResponseLike) {
+  const app = await loadApp();
+  return app(req, res);
+}
